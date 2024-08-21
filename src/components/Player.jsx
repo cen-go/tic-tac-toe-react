b613cfb776782971837a49bf25bd9f6c3ff0b4e8
luzumsuzadam <cengizulutas123123@gmail.com>
2024-08-21 23:13:07 +0300

    21 August

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function handleClick() {
+    setIsEditing(prevIsEditing => !prevIsEditing);
+  }
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" value={playerName} onChange={handleChange} />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
